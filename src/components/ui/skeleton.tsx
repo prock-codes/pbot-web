1e3bfad9d851d9c5e9aedfcd3cfefb655e612480
root <root@vultr.guest>
2026-01-31 10:26:45 +0000

    fix: add style prop support to Skeleton component

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -1,16 +1,17 @@
 import { cn } from '@/lib/utils';
 
-interface SkeletonProps {
+interface SkeletonProps extends React.HTMLAttributes<HTMLDivElement> {
   className?: string;
 }
 
-export function Skeleton({ className }: SkeletonProps) {
+export function Skeleton({ className, ...props }: SkeletonProps) {
   return (
     <div
       className={cn(
         'relative overflow-hidden rounded bg-discord-lighter/50',
         className
       )}
+      {...props}
     >
       <div className="absolute inset-0 -translate-x-full animate-[shimmer_2s_infinite] bg-gradient-to-r from-transparent via-white/5 to-transparent" />
     </div>
